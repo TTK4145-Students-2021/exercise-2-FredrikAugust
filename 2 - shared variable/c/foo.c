8b49405a16758bf9d8200b50852fe78b553bdc66
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-07 21:52:35 +0100

    Solve C exercise (1)

--- 2 - shared variable/c/foo.c
+++ 2 - shared variable/c/foo.c
@@ -3,33 +3,48 @@
 
 int i = 0;
 
-// Note the return type: void*
-void* incrementingThreadFunction(){
-    for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i++;
+/**
+ * Reasoning:
+ *   We use a mutex because we want to simply keep track of whether or not the thread has access to our resource.
+ *   We could also use a binary semaphore, but a mutex is more semantically correct, and cleaner.
+ */
+pthread_mutex_t lock;
+
+void *incrementingThreadFunction(void* num) {
+    for (int j = 0; j < 11337; j++) {
+        pthread_mutex_lock(&lock);   // Acquire lock
+        (*(int*)num)++;              // Critical work
+        pthread_mutex_unlock(&lock); // Release lock
     }
+
     return NULL;
 }
 
-void* decrementingThreadFunction(){
-    for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i--;
+void *decrementingThreadFunction(void* num) {
+    for (int j = 0; j < 10000; j++) {
+        pthread_mutex_lock(&lock);
+        (*(int*)num)--;
+        pthread_mutex_unlock(&lock);
     }
+
     return NULL;
 }
 
 
-int main(){
+int main() {
+    pthread_mutex_init(&lock, NULL);
+
     pthread_t incrementingThread, decrementingThread;
-    
-    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
-    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, &i);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, &i);
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
+
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
